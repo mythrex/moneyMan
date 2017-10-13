25f0c0433138ffcf60c9b925ddd4a81a1d9e7e4a
mythrex <shivam.kaushik73@gmail.com>
2017-10-13 18:01:51 +0530

    Done Adding PG_SIMPLE for session store

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
+const dotenv = require('dotenv').load();
 const Sequelize = require('sequelize');
 //connect to db
+
 //TODO setup-the project and db
-const db = new Sequelize("db","username","pass",{
-  dialect: "dialect",
-  host: "localhost"
+const db = new Sequelize(process.env.DB_DB,process.env.DB_USER,process.env.DB_PASS,{
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  operatorsAliases: false
 });
 
-
-
+db.sync().then(()=>{
+	console.log('DB Connected');
+}).catch((err)=>{
+	throw err;
+});
 
 module.exports = db;
---
+++ config/passport.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const facebookStrategy = require('passport-facebook')
+const OAUTH = require('../models/users.js');
+const dotenv = require('dotenv').config();
+
+passport.serializeUser((user,done)=>{
+	done(null,user.network_id);
+})
+
+passport.deserializeUser((id,done)=>{
+	OAUTH.find({
+		where: {
+			network_id: id
+		}
+	}).then((user)=>{
+		done(null,user);
+	}).catch((err)=>{
+		done(err);
+	})
+})
+
+let FacebookStrategy = new facebookStrategy({
+	clientID: process.env.FACEBOOK_CLIENT_ID,
+	clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+	callbackURL: 'http://localhost:3000/auth/facebook/callback'
+},(accessToken, refreshToken, profile, done)=>{
+	OAUTH.findOrCreate({
+		where: {
+			network_id: profile.id
+		},
+		defaults: {
+			network: profile.provider,
+			name: profile.displayName,
+			email: profile.email
+		}
+	}).spread((user,created)=>{
+		done(null,user);
+	}).catch((err)=>{
+		done(err);
+	});
+});
+
+passport.use('facebook',FacebookStrategy);
+
+module.exports = passport;
